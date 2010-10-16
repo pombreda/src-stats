18521a69592e4c25f316fda6544f3a96bfb9ddf4
mhallin <mhallin@gmail.com>
2010-10-16 14:55:44 +0200

    Refactored count-lines to use optparse.

--- src/srcstats/countlines_cmd.py
+++ src/srcstats/countlines_cmd.py
@@ -3,49 +3,87 @@
 import sys
 import math
 
+from optparse import OptionParser
+
 from languages import Language
-from linecounter import count_file
+from linecounter import count_file, Result
 from table import Table, Column
 
+
+parser = OptionParser('usage: %prog [options] file [file ...]')
+parser.add_option('-l', '--by-language',
+                  help='Order result by language instead of by file',
+                  default=False,
+                  action="store_true")
+
+def add_results(dest, src):
+    dest.source += src.source
+    dest.comment += src.comment
+    dest.blank += src.blank
+
+def sum_result(result):
+    return result.source + result.comment + result.blank
+
+def print_result_set(table, header, result):
+    print table.row((header,
+                     result.source,
+                     result.comment,
+                     result.blank,
+                     sum_result(result)))
+
+def print_totals(table, totals):
+    print table.separator()
+    print_result_set(table, 'Total', totals)
+
+def print_language_stats(table, language_stats):
+    items = language_stats.items()
+    items.sort(cmp=lambda x, y: cmp(sum_result(x[1]),
+                                    sum_result(y[1])),
+               reverse=True)
+    
+    for lang, stats in items:
+        print_result_set(table, lang.name, stats)
+
 def main_func():
-    if len(sys.argv) == 1:
-        print >>sys.stderr, 'Usage: %s filename [filename ...]'
+    (options, args) = parser.parse_args()
+
+    if not args:
+        parser.print_usage()
         return 1
 
     table = Table((Column('Filename', align='l'),
                    Column('Source', 9, align='r'),
-                   Column('Blank', 9, align='r'),
-                   Column('Total', 9, align='r')))
-
+                   Column('Comments', 11, align='r'),
+                   Column('Blank', 8, align='r'),
+                   Column('Total', 11, align='r')))
+    
     print table.header()
     print table.separator()
 
-    source_total = 0
-    comment_total = 0
-    blank_total = 0
+    totals = Result()
+
+    language_stats = {}
 
     for filename in sys.argv[1:]:
         lang = Language.from_filename(filename)
 
         if not lang:
             continue
-        
+
         result = count_file(open(filename), lang)
 
-        source_total += result.source
-        comment_total += result.comment
-        blank_total += result.blank
+        add_results(totals, result)
 
-        print table.row((filename,
-                         result.source,
-                         result.comment,
-                         result.blank,
-                         result.source + result.comment + result.blank))
+        if options.by_language:
+            if lang not in language_stats:
+                language_stats[lang] = Result()
+
+            add_results(language_stats[lang], result)
+        else:
+            print_result_set(table, filename, result)
+
+    if options.by_language:
+        print_language_stats(table, language_stats)
 
     if len(sys.argv) > 2:
-        print table.separator()
-        print table.row(('Total',
-                         source_total,
-                         comment_total,
-                         blank_total,
-                         source_total + comment_total + blank_total))
+        print_totals(table, totals)
